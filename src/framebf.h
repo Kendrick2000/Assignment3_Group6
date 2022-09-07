4086f66d7347d8b6712ecb5bf2322fb10d50b020
Kendrick2000 <56669029+Kendrick2000@users.noreply.github.com>
2022-09-07 13:58:44 +0700

    update font, ball, drawString, drawChar to 32x32

--- src/framebf.h
+++ src/framebf.h
@@ -1,6 +1,12 @@
 // ----------------------------------- framebf.h ------------------------------------- 
 void framebf_init(); 
+void framebf_blank();
+void framebf_release();
 void drawPixelARGB32(int x, int y, unsigned int attr); 
 void drawRectARGB32(int x1, int y1, int x2, int y2, unsigned int attr, int fill); 
 void drawChar(int x, int y, char c, unsigned char attr);
 void drawString(int x, int y, char *s, unsigned int attr);
+void drawChar32x32(int x, int y, char c, unsigned char attr);
+void drawString32x32(int x, int y, char *s, unsigned int attr);
+void drawCharLarge(int x, int y, char c, unsigned char attr);
+void drawStringLarge(int x, int y, char *s, unsigned int attr);
