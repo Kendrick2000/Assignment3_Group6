fd70ef45bbbb5403f3423fd18a693164cd533dd4
uyennp <45529431+uyennp@users.noreply.github.com>
2022-09-08 13:43:11 +0700

    update collision function with tiles

--- src/game.h
+++ src/game.h
@@ -1,7 +1,8 @@
-void collision_check();
 void draw_pixelBall();
 void draw_greenTile();
 void draw_blueTile();
 void draw_yellowTile();
 void draw_redTile();
 void draw_paddle(int x, int y);
+void collision_tile();
+void del_tile(int x, int y);
